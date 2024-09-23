f4a087db6528bb9c2d947154c4c09f3c4ac36c42
yaroslavtykhonchuk <yaroslav.tykhonchuk@youscan.io>
2024-09-23 18:38:39 +0300

    Update header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,15 +1,16 @@
-import youScanLogo from './assets/logo.svg'
-import './Header.css'
+import youScanLogo from './assets/favicon.svg';
 
 function Header() {
     return (
-        <div className="header">
-            <a href="https://youscan.io" target="_blank">
-                <img src={youScanLogo} className="logo" alt="Youscan logo" />
-            </a>
-            <h1 className="title">Brand Index</h1>
-        </div>
-    )
+        <header className="fixed top-0 left-0 right-0 bg-white shadow-md z-10">
+            <div className="container mx-auto px-4 py-3 flex justify-center items-center">
+                <a href="https://youscan.io" target="_blank" rel="noopener noreferrer" className="mr-4">
+                    <img src={youScanLogo} className="h-8 w-auto" alt="Youscan logo" />
+                </a>
+                <h1 className="text-2xl font-bold text-gray-800">Brand Index</h1>
+            </div>
+        </header>
+    );
 }
 
-export default Header
+export default Header;
